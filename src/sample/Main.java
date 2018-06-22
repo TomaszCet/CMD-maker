732cfcda3ff1a787c4f35141abd80c6dba9b5958
Tomasz Cetnar <tomekcet@gmail.com>
2018-06-22 17:25:14 +0200

    initial view created

---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        int height = 425;
+        int width = 600;
+        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
+        primaryStage.setTitle("CMD Maker");
+        primaryStage.setScene(new Scene(root, 600, 425));
+        primaryStage.setMinHeight(height+50);
+        primaryStage.setMinWidth(width+20);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/conctroller/Controller.java
@@ -0,0 +1,4 @@
+package sample.conctroller;
+
+public class Controller {
+}
